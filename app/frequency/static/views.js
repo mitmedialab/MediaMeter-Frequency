4bbe63dc13503d094e3c7d2ca56cd459e76a69b3
Edward L Platt <ed@elplatt.com>
2014-06-26 15:06:30 -0400

    Add skeleton for result view.

--- app/frequency/static/views.js
+++ app/frequency/static/views.js
@@ -5,5 +5,23 @@ App.FrequencyResultListView = Backbone.View.extend({
     },
     render: function () {
         App.debug('App.FrequencyResultListView.render()');
+        this.collection.each(function (m) {
+            var view = new App.FrequencyResultView({
+                model: m
+            });
+            this.$el.append(view.el);
+        }, this);
+    }
+});
+
+App.FrequencyResultView = Backbone.View.extend({
+    template: _.template($('#tpl-frequency-result-view').html()),
+    initialize: function (options) {
+        this.render();
+    },
+    render: function () {
+        App.debug('App.FrequencyResultView.render()');
+        this.$el.html(this.template());
+        this.$('.frequency-result-view-content').html('Hello, World!');
     }
 });
